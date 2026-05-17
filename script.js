82c35cfa3abdee15f256cb2693c778dd8fcf0db6
dmahamna12-maker <dmahamna12@gmail.com>
2026-05-17 20:54:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* Typed JS */
+const typed = new Typed('.multiple-text', {
+    strings: [
+        
+        'Backend Developer',
+        'Web Designer',
+        'Frontend Developer',
+    ],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+});
